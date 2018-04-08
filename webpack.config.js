d8c9355b73c585bfa2c09f07138245d422579a9e
E-Liang Tan <eliang@eliangtan.com>
2018-04-08 11:38:23 +0800

    Upgrade all dependencies to fix type error
    Couldn't deploy earlier due to some type errors, so I just upgraded
    everything. This commit can be deployed (after a .env file is created).
    
    The switch to Webpack 4 required a new webpack.config.js. This was
    largely obtained from a serverless-webpack example, but adapted for
    TypeScript.
    
    Webpack config that we shamelessly adapted heavily:
    https://github.com/serverless-heaven/serverless-webpack/blob/5cf24795fe1501e2e7c71dfe60368fc6578efd25/examples/babel-webpack-4/webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,41 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: slsw.lib.entries,
-  devtool: 'source-map',
+  target: 'node',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false,
+  },
+  performance: {
+    // Turn off size warnings for entry points
+    hints: false,
+  },
   resolve: {
-    extensions: [
-      '.js',
-      '.jsx',
-      '.json',
-      '.ts',
-      '.tsx'
-    ]
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  devtool: 'nosources-source-map',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
   },
   output: {
-    libraryTarget: 'commonjs',
+    libraryTarget: 'commonjs2',
     path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
-  },
-  target: 'node',
-  module: {
-    loaders: [
-      { test: /\.ts(x?)$/, loader: 'ts-loader' },
-    ],
+    sourceMapFilename: '[file].map',
   },
 };
