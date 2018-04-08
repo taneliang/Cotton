c91b328998626f3543514f2cbc8daf4a38d083d3
E-Liang Tan <eliang@eliangtan.com>
2018-04-08 17:04:02 +0800

    Add Octokit

--- src/handlers/restTrigger.ts
+++ src/handlers/restTrigger.ts
@@ -1,16 +1,30 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import * as Octokit from '@octokit/rest';
 
-export const restTrigger: Handler = (
+export const restTrigger: Handler = async (
   event: APIGatewayEvent,
   context: Context,
   callback: Callback,
 ) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      input: event,
-    }),
-  };
+  const octokit = new Octokit();
+  let response = null;
+
+  try {
+    // const result = await octokit.apps.get({});
+    const result = await octokit.misc.getRepoLicense({
+      owner: 'nusmodifications',
+      repo: 'nusmods',
+    });
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        result,
+      }),
+    };
+  } catch (e) {
+    return callback(e);
+  }
 
   return callback(null, response);
 };
