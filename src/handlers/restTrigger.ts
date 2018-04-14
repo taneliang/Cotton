9fc485304c05f5b50ee2a5101c016afc63571cb4
E-Liang Tan <eliang@eliangtan.com>
2018-04-14 13:08:40 +0800

    Implement package.json upgrades and one-file commit
    Only one file can be committed, as the SHAs of the other files will
    change after the first file is uploaded, causing the second upload to
    fail.

--- src/handlers/restTrigger.ts
+++ src/handlers/restTrigger.ts
@@ -1,11 +1,12 @@
 import { dirname, join } from 'path';
-import { writeFile } from 'fs';
+import { readFile, writeFile } from 'fs';
 import { promisify } from 'util';
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
 import * as Octokit from '@octokit/rest';
 import * as _ from 'lodash';
 import 'lodash.product';
 import * as mkdirp from 'mkdirp';
+import * as ncu from 'npm-check-updates';
 
 import generateGitHubToken from '../auth/generateToken';
 
@@ -17,9 +18,17 @@ type PathPair = {
   localPath: string;
 };
 
+type PackageJson = {
+  name: string;
+  version: string;
+  dependencies: { readonly [index: string]: string } | undefined;
+  devDependencies: { readonly [index: string]: string } | undefined;
+};
+
 // Using 2 different versions of promisify to get around compile
 // error: TS2554: Expected X arguments, but got Y.
 const mkdirpAsync = Promise.promisify(mkdirp);
+const readFileAsync = promisify(readFile);
 const writeFileAsync = promisify(writeFile);
 
 async function fetchInstallationIds(octokit: Octokit) {
@@ -68,6 +77,34 @@ function fetchFiles(owner: string, repo: string, filePaths: PathPair[], octokit:
   );
 }
 
+// TODO: Handle ignored packages
+async function upgradeProject(rootDir: string) {
+  // Read package.json
+  const packageJsonPath = join(rootDir, 'package.json');
+  const packageJson = await readFileAsync(packageJsonPath, 'utf-8');
+  // console.log('before', packageJsonString);
+
+  // TODO: Calculate diff
+  // const package = JSON.parse(packageJson) as PackageJson;
+
+  // TODO: Ensure that custom packages are ignored
+  const upgradedPackage = await ncu.run({
+    packageFile: packageJsonPath,
+    silent: true,
+    jsonAll: true,
+    upgradeAll: true,
+  });
+
+  // Save new package.json
+  // TODO: Preserve formatting (only replace necessary bits? Prettier?)
+  await writeFileAsync(packageJsonPath, JSON.stringify(upgradedPackage, null, 2), 'utf-8');
+
+  // TODO: Run yarn to update yarn.lock
+
+  // TODO: Return diff
+  return upgradedPackage;
+}
+
 // Upgrade a repository. octokit should be authenticated with token to access repo.
 async function upgradeRepository(repoDetails: any, octokit: Octokit) {
   console.log('Upgrading repository', repoDetails.full_name);
@@ -101,15 +138,41 @@ async function upgradeRepository(repoDetails: any, octokit: Octokit) {
   const filePaths = getFilePaths(projDirPaths, ['package.json', 'yarn.lock']);
   await fetchFiles(repoDetails.owner.login, repoDetails.name, filePaths, octokit);
 
-  // TODO: Upgrade all package.jsons
+  // TODO: Upgrade projects
+  await Promise.map(projDirPaths, (projDir: PathPair) => upgradeProject(projDir.localPath));
 
-  // TODO: Run yarn for all package.jsons
+  // TODO: Commit all files. Gotta fetch new SHAs I think.
+  // TODO: Don't commit unchanged files.
+  const fileDetailsKeyedByPath = _.keyBy(
+    [...packageJsons.data.items, ...yarnLocks.data.items],
+    'path',
+  );
+  const fileContents = await Promise.map(filePaths, (path: PathPair) =>
+    readFileAsync(path.localPath, 'base64'),
+  );
+  const filesToCommit = filePaths.map((path: PathPair, idx: number) => ({
+    ...path,
+    sha: fileDetailsKeyedByPath[path.repoPath].sha,
+    contents: fileContents[idx],
+  }));
 
-  // TODO: Commit all files
+  await Promise.all(
+    filesToCommit.map((file: PathPair & { sha: string; contents: string }) => {
+      console.log('Upgrading file', file.repoPath, file.sha);
+      return octokit.repos.updateFile({
+        owner: repoDetails.owner.login,
+        repo: repoDetails.name,
+        path: file.repoPath,
+        message: `Upgrade ${file.repoPath}`,
+        content: file.contents,
+        sha: file.sha,
+      });
+    }),
+  );
 
   // TODO: Submit or edit PR
 
-  const result = { projDirPaths, filePaths };
+  const result = { fileDetailsKeyedByPath, filesToCommit, projDirPaths, filePaths };
   return result;
 }
 
