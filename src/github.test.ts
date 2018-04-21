0c48bf48216ad1a8361396c8e93198591c003c82
E-Liang Tan <eliang@eliangtan.com>
2018-04-21 17:19:33 +0800

    Write tests for 2 github helper functions

---
+++ src/github.test.ts
@@ -0,0 +1,36 @@
+import { fetchInstallationIds, fetchTokenForInstallation } from './github';
+import * as Octokit from '@octokit/rest';
+
+// Cleanly double cast an octokitMock object to Octokit to appease TypeScript
+function mockOctokit(octokitMock: any): Octokit {
+  return octokitMock as Octokit;
+}
+
+describe('github helpers', () => {
+  beforeEach(() => jest.resetAllMocks());
+  afterEach(() => jest.resetAllMocks());
+
+  describe(fetchInstallationIds, () => {
+    test('should return installation IDs', async () => {
+      const apiResult = require('./__mocks__/octokit.apps.getInstallations.valid.json');
+      const octokit = mockOctokit({
+        apps: {
+          getInstallations: jest.fn().mockResolvedValue({ data: apiResult }),
+        },
+      });
+      await expect(fetchInstallationIds(octokit)).resolves.toEqual([1]);
+    });
+  });
+
+  describe(fetchTokenForInstallation, () => {
+    test('should return token', async () => {
+      const apiResult = require('./__mocks__/octokit.apps.createInstallationToken.valid.json');
+      const octokit = mockOctokit({
+        apps: {
+          createInstallationToken: jest.fn().mockResolvedValue({ data: apiResult }),
+        },
+      });
+      await expect(fetchTokenForInstallation('1', octokit)).resolves.toEqual(apiResult.token);
+    });
+  });
+});
